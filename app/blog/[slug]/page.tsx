b252eb80f71e91783e608517fe4696cbca549acc
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-17 21:57:01 +0530

    use of roter on details page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
 
-const pageBlogDetails = () => {
-  return <div>pageBlogDetails</div>;
+import { useRouter } from "next/navigation";
+
+const PageBlogDetails = () => {
+  const router = useRouter();
+  console.log("🚀 router:", router);
+
+  return (
+    <>
+      <div>pageBlogDetails</div>
+      <button onClick={() => router.push("/")}>Home</button>
+    </>
+  );
 };
 
-export default pageBlogDetails;
+export default PageBlogDetails;
