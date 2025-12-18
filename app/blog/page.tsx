c67b1fdada3fa768938cbbf4f1eaa7b4657a5042
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-18 13:22:32 +0530

    showing of title and description

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 export default function BlogPage() {
   return <div>BlogPage</div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
 
+export const metadata = {
+  title: "Dj Events",
+  description: "Welcome to Dj Events",
+};
+
 export default function Page() {
   return (
     <>
