1b53b92432c2bdf9ee7bf7670bd5c4626e21b8c7
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-19 10:33:12 +0530

    main page cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import Link from "next/link";
-import Layout from "./layout";
-
 export default function Page() {
-  return (
-    <>
-      <h1>Hello Next.js!</h1>
-      <Link href="/blog">Go to blog</Link>
-    </>
-  );
+  return <></>;
 }
